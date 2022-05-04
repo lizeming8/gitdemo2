dbfb5c05d79185268792e04b18406ecf109f2e4b
lizeming <2066016547@qq.com>
2022-05-04 21:50:59 +0800

    first

---
+++ src/main/java/com/lizeming/git/GitTest.java
@@ -0,0 +1,9 @@
+package com.lizeming.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("helll");
+        System.out.println("helll");
+        System.out.println("helll");
+    }
+}
